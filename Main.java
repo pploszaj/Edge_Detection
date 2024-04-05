a1782a5a22aafacfe28bff8e2fbaa5137572019f
Patryk Ploszaj <patryk.ploszaj07@gmail.com>
2024-04-05 19:52:19 -0400

    Complete project

--- Main.java
+++ Main.java
@@ -1,15 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-//    Step 1: loadImage (inFile, MFAry)
-//    Step 2: imgReformatPrettyPrint (MFAry, outFile1)
-//    Step 3: zeroCrossingEdgeDetector (MFAry, edgeAry, mask, zeroCrossAry, zeroCrossFile, binEdgeFile, deBugFile)
-//    Step 4: close all files
 
         if (args.length < 5) {
             System.out.println("Incorrect number of arguments");
@@ -40,8 +33,8 @@ public class Main {
 
         Edge edge = new Edge(numRows, numCols, minVal, maxVal);
         edge.loadImage(inFile);
-        edge.zeroReformatPrettyPrint(outFile);
-        edge.zeroCrossingEdgeDetector();
+        edge.imgReformatPrettyPrint(edge.MFArray, outFile);
+        edge.zeroCrossingEdgeDetector(zeroCrossFile, binEdgeFile, debugFile);
 
     }
 }
@@ -69,7 +62,7 @@ class Edge {
             {0, -2, 0},
             {-2, 8, -2},
             {0, -2, 0},
-        }
+        };
     }
 
     public void loadImage(String inFile){
@@ -81,7 +74,7 @@ class Edge {
             for (int i = 2; i < numRows + 2; i++) {
                 for (int j = 2; j < numCols + 2; j++) {
                     if (scanner.hasNextInt()) {
-                        this.MFArray[i][j] = scanner.nextInt();
+                        MFArray[i][j] = scanner.nextInt();
                     }
                 }
             }
@@ -91,7 +84,153 @@ class Edge {
         }
     }
 
-    public void zeroCrossingEdgeDetector(){}
+    public void zeroCrossingEdgeDetector(String zeroCrossFile, String binEdgeFile, String debugFile){
+        computeEdge(debugFile);
+        crossZero(debugFile);
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(zeroCrossFile, true));
+            writer.println(numRows + " " + numCols + " " + "0" + " " + "1");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+        imgReformatPrettyPrint(zeroCrossArray, zeroCrossFile);
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(binEdgeFile, true));
+            writer.println(numRows + " " + numCols + " " + "0" + " " + "1");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+        binaryEdgeDetector(zeroCrossArray, binEdgeFile);
+
+
+    }
+
+    public void computeEdge(String debugFile){
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(debugFile, true));
+            writer.println("Entering computeEdge() method");
+
+
+            for (int i = 1; i < numRows; i++) {
+                for (int j = 1; j < numCols; j++) {
+                    edgeArray[i][j] = convolution3x3(i, j);
+                }
+            }
+
+            writer.println("Leaving computeEdge() method");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
+
+    public void crossZero(String debugFile){
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(debugFile, true));
+            writer.println("Entering crossZero() method");
+
+
+            for (int i = 1; i < numRows; i++) {
+                for (int j = 1; j < numCols; j++) {
+                    if (edgeArray[i][j] <= 0) {
+                        zeroCrossArray[i][j] = 0;
+                    } else {
+                        zeroCrossArray[i][j] = 1;
+                    }
+                }
+            }
+            writer.println("Leaving crossZero() method");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file: " + e.getMessage());
+        }
+    }
+
+    public void imgReformatPrettyPrint(int[][] inArray, String outFile){
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(outFile, true));
+            writer.println(numRows + " " + numCols + " " + minVal + " " + maxVal);
+
+            String str = Integer.toString(maxVal);
+            int width = str.length();
+
+            for (int i = 2; i < numRows + 2; i++) {
+                for (int j = 2; j < numCols + 2; j++) {
+                    if(inArray[i][j] > 0){
+                        writer.print(inArray[i][j]);
+                    } else {
+                        writer.print('.');
+                    }
+                    String valueStr = Integer.toString(inArray[i][j]);
+                    int valueWidth = valueStr.length();
+                    while(valueWidth <= width) {
+                        writer.print(" ");
+                        valueWidth++;
+                    }
+                }
+                writer.println();
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the output file: " + e.getMessage());
+        }
+    }
+
+    public int convolution3x3(int i, int j){
+        int sum = 0;
+        for (int maskRow = 0; maskRow < 3; maskRow++) {
+            for (int maskCol = 0; maskCol < 3; maskCol++) {
+                int row = i + maskRow - 1;
+                int col = j + maskCol - 1;
+                sum += MFArray[row][col] * mask[maskRow][maskCol];
+            }
+        }
+        return sum;
+
+    }
+
+    public void binaryEdgeDetector(int[][] zeroCrossArray, String binEdgeFile){
+        try {
+            PrintWriter writer = new PrintWriter(new FileWriter(binEdgeFile));
+
+            writer.println(numRows + " " + numCols + " 0 1");
+
+            for (int i = 1; i <= numRows; i++) {
+                for (int j = 1; j <= numCols; j++) {
+                    if (isBinaryEdge(zeroCrossArray, i, j)) {
+                        writer.print("1 ");
+                    } else {
+                        writer.print("0 ");
+                    }
+                }
+                writer.println();
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the binary edge file: " + e.getMessage());
+        }
+
+    }
+
+    private boolean isBinaryEdge(int[][] array, int i, int j) {
+        if (array[i][j] < 1) return false;
+
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+            for (int colOffset = -1; colOffset <= 1; colOffset++) {
+                int neighborRow = i + rowOffset;
+                int neighborCol = j + colOffset;
+
+                if (rowOffset == 0 && colOffset == 0) continue;
+
+                if (array[neighborRow][neighborCol] == 0) return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 
